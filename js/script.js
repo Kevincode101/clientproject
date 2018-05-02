1b34b2da6813e783d5c84bfbc9da74bff5611086
kevin lima <kevin.lima.code@gmail.com>
2018-05-02 21:28:44 +0000

    The Images without N/A no longer appear

--- js/script.js
+++ js/script.js
@@ -4,32 +4,34 @@
 
 /* global $ */
 
-$( document ).ready( function() {
-    
-	$( "#srch-term" ).keyup( function( event ) {
-		if ( event.keyCode === 13 ) {
-			$( "#submit" ).click();
-		}
-	} );   
+$(document).ready(function() {
 
-	$( "#submit" ).click( function() {
-	    http://www.omdbapi.com/?apikey=90d4b10a&y=2016&s=dory
-		$( ".gallery" ).empty();
-		var userInput = $( '#srch-term' ).val();
-		var url = "https://omdbapi.com?apikey=90d4b10a&s=" + userInput.toLowerCase(); + "&plot=short";
-		$.ajax( {
+	$("#srch-term").keyup(function(event) {
+		if (event.keyCode === 13) {
+			$("#submit").click();
+		}
+	});
+
+	$("#submit").click(function() {
+		$(".gallery").empty();
+		var userInput = $('#srch-term').val();
+		var url = "https://omdbapi.com?apikey=90d4b10a&s=" + userInput.toLowerCase();
+		$.ajax({
 			url: url,
 			method: "GET",
-			success: function( response ) {
-				$( ".giphyTitle" ).html( "<h1 id='movieTitle'>" + userInput.toLowerCase() + "</h1>" );
-				for ( var i = 0; i < response.Search.length; i++ ) {
-					$( '.gallery' ).append(
-						'<img data-toggle="modal" data-target="#modal-' + i + '" class="col-md-3 giphy" src=' + response.Search[i].Poster + '>');
-					console.log("The amount of Movies being displayed to the screen: " + response.Search.length);
-					console.log("The total amount of possible Movies " + response.Search[1].totalResults);
+			success: function(response) {
+				$(".giphyTitle").html("<h1 id='movieTitle'>" + userInput + "</h1>");
+				for (var i = 0; i < response.Search.length; i++) {
+					if (response.Search[i].Poster !== "N/A") {
+						$('.gallery').append(
+							'<img data-toggle="modal" data-target="#modal-' + i + '" class="col-md-3 movies" src=' + response.Search[i].Poster + '>'
+						);
+						console.log("The amount of Movies being displayed to the screen: " + response.Search.length);
+						console.log("The total amount of possible Movies " + response.Search[1].totalResults);
+					}
 				}
-				$( "footer" ).css( "margin-top", 100 );
+				$("footer").css("margin-top", 100);
 			},
-		} );
-	} );
-} );
+		});
+	});
+});
